559b33f7b3f901068376024fec9e9717240d9303
JOÃO MIGUEL DOS SANTOS SILVA <joao.silva3727@etec.sp.gov.br>
2026-05-18 13:54:48 -0300

    primeira versão do projeto

---
+++ script.js
@@ -0,0 +1,12 @@
+
+const novaSolicitacao = document.getElementById("novaSolicitacao");
+const minhasSolicitacoes = document.getElementById("minhasSolicitacoes");
+
+novaSolicitacao.addEventListener("click", () => {
+    alert("Tela de nova solicitação");
+});
+
+minhasSolicitacoes.addEventListener("click", () => {
+    alert("Tela de minhas solicitações");
+});
+
